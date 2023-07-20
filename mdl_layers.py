225c90a8a2da8a46ca040a003d93bc7eda449d8c
cj-torres <52362149+cj-torres@users.noreply.github.com>
2023-07-19 17:03:39 -0700

    bug fixes and support for block sampling

--- mdl_layers.py
+++ mdl_layers.py
@@ -179,7 +179,8 @@ class RegLSTM(torch.nn.Module):
         #self._all_weights = []
         for layer in range(num_layers):
             for direction in range(num_directions):
-                w_ih = RegularizedParam(torch.nn.Parameter(torch.empty((gate_sz, input_sz))), lam, weight_decay=self.wd)
+                layer_input_size = self.input_size if layer == 0 else self.hidden_size * num_directions
+                w_ih = RegularizedParam(torch.nn.Parameter(torch.empty((gate_sz, layer_input_size))), lam, weight_decay=self.wd)
                 w_hh = RegularizedParam(torch.nn.Parameter(torch.empty((gate_sz, hidden_sz))), lam, weight_decay=self.wd)
                 b_ih = RegularizedParam(torch.nn.Parameter(torch.empty(gate_sz)), lam, is_bias=True, weight_decay=self.wd)
                 b_hh = RegularizedParam(torch.nn.Parameter(torch.empty(gate_sz)), lam, is_bias=True, weight_decay=self.wd)
@@ -202,6 +203,10 @@ class RegLSTM(torch.nn.Module):
         for reg_param in self._weights:
             reg_param.reset_parameters()
 
+    def constrain_parameters(self):
+        for reg_param in self._weights:
+            reg_param.constrain_parameters()
+
     def set_lam(self, lam: float):
         self.lam = lam
         for w in self._weights:
@@ -220,9 +225,128 @@ class RegLSTM(torch.nn.Module):
     def count_l2(self):
         return torch.stack([w.count_l2() for w in self._weights]).sum()
 
-    def forward(self, seq, hx=None):
+    def forward(self, seq, hx=None, block_sample=False):
         assert (seq.dim() in (2, 3)), f"LSTM: Expected input to be 2-D or 3-D but received {seq.dim()}-D tensor"
-        #breakpoint()
+        is_batched = seq.dim() == 3
+        batch_dim = 0
+        if is_batched:
+            batch_sz = seq.size(batch_dim)
+        else:
+            batch_sz = 1
+        if not is_batched:
+            seq = seq.unsqueeze(batch_dim)
+
+        if hx is None:
+            num_directions = 2 if self.bidirectional else 1
+            batches = 1 if self.training and not block_sample else batch_sz
+            h_zeros = torch.zeros(self.num_layers * num_directions,
+                                  batches, self.hidden_size,
+                                  dtype=seq.dtype, device=seq.device)
+            c_zeros = torch.zeros(self.num_layers * num_directions,
+                                  batches, self.hidden_size,
+                                  dtype=seq.dtype, device=seq.device)
+            hx = (h_zeros, c_zeros)
+
+        if self.training:
+            outputs = []
+            hiddens = []
+            samples = zip(*[w.sample_weights(torch.Size([batch_sz]), self.training) for w in self._weights])
+            for x, weights in zip(seq, samples):
+                if x.is_cuda and all([weight.is_cuda for weight in weights]):
+                    torch._cudnn_rnn_flatten_weight(
+                        weights, 4,
+                        self.input_size, int(_cudnn.RNNMode.lstm),
+                        self.hidden_size, 0, self.num_layers,
+                        True, bool(self.bidirectional))
+                result = torch._VF.lstm(x.unsqueeze(dim=batch_dim), hx, weights, True, self.num_layers,
+                                        0, self.training, self.bidirectional, True)
+                outputs.append(result[0])
+                hiddens.append(result[1:])
+            output = torch.cat(outputs, dim=batch_dim)
+            hidden = (torch.stack([h[0].squeeze(1) for h in hiddens], dim=batch_dim),
+                      torch.stack([h[1].squeeze(1) for h in hiddens], dim=batch_dim))
+        else:
+            samples = (w.sample_weights(torch.Size([batch_sz]), self.training) for w in self._weights)
+            result = torch._VF.lstm(seq, hx, tuple(samples), True, self.num_layers,
+                                    0, self.training, self.bidirectional, True)
+            output = result[0]
+            hidden = result[1:]
+
+        if not is_batched:
+            output = output.squeeze(batch_dim)
+            hidden = (hidden[0], hidden[1])
+
+        return output, hidden
+
+
+class RegRNN(torch.nn.Module):
+
+    def __init__(self, input_sz: int, hidden_sz: int, lam: float, num_layers: int = 1,
+                 bidirectional: bool = False, weight_decay: float = 0, nonlinearity="tanh"):
+        super(RegRNN, self).__init__()
+        gate_sz = hidden_sz
+        self.bidirectional = bidirectional
+        self.num_layers = num_layers
+        self.hidden_size = hidden_sz
+        self.input_size = input_sz
+        self.lam = lam
+        self.wd = weight_decay
+        if nonlinearity != "tanh" or nonlinearity != "relu":
+            raise ValueError("Unknown nonlinearity '{}'".format(self.mode))
+        self.mode = nonlinearity
+
+        num_directions = 2 if bidirectional else 1
+
+        self._weights_names = []
+        for layer in range(num_layers):
+            for direction in range(num_directions):
+                layer_input_size = self.input_size if layer == 0 else self.hidden_size * num_directions
+                w_ih = RegularizedParam(torch.nn.Parameter(torch.empty((gate_sz, layer_input_size))), lam, weight_decay=self.wd)
+                w_hh = RegularizedParam(torch.nn.Parameter(torch.empty((gate_sz, hidden_sz))), lam, weight_decay=self.wd)
+                b_ih = RegularizedParam(torch.nn.Parameter(torch.empty(gate_sz)), lam, is_bias=True, weight_decay=self.wd)
+                b_hh = RegularizedParam(torch.nn.Parameter(torch.empty(gate_sz)), lam, is_bias=True, weight_decay=self.wd)
+
+                layer_params = (w_ih, w_hh, b_ih, b_hh)
+                suffix = '_reverse' if direction == 1 else ''
+                param_names = ['weight_ih_l{}{}', 'weight_hh_l{}{}', 'bias_ih_l{}{}', 'bias_hh_l{}{}']
+                param_names = [x.format(layer, suffix) for x in param_names]
+
+                for name, param in zip(param_names, layer_params):
+                    setattr(self, name, param)
+                self._weights_names.extend(param_names)
+
+        self._weights = [getattr(self, wn) if hasattr(self, wn) else None
+                              for wn in self._weights_names]
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for reg_param in self._weights:
+            reg_param.reset_parameters()
+
+    def constrain_parameters(self):
+        for reg_param in self._weights:
+            reg_param.constrain_parameters()
+
+    def set_lam(self, lam: float):
+        self.lam = lam
+        for w in self._weights:
+            w.set_lam(lam)
+
+    def set_wd(self, wd):
+        for w in self._weights:
+            w.set_wd(wd)
+
+    def regularization(self):
+        return torch.stack([w.regularization() for w in self._weights]).sum()
+
+    def count_l0(self):
+        return torch.stack([w.count_l0() for w in self._weights]).sum()
+
+    def count_l2(self):
+        return torch.stack([w.count_l2() for w in self._weights]).sum()
+
+    def forward(self, seq, hx=None, block_sample=False):
+        assert (seq.dim() in (2, 3)), f"LSTM: Expected input to be 2-D or 3-D but received {seq.dim()}-D tensor"
         is_batched = seq.dim() == 3
         batch_dim = 0
         if is_batched:
@@ -234,48 +358,64 @@ class RegLSTM(torch.nn.Module):
 
         if hx is None:
             num_directions = 2 if self.bidirectional else 1
+            batches = 1 if self.training and not block_sample else batch_sz
             h_zeros = torch.zeros(self.num_layers * num_directions,
-                                  1, self.hidden_size,
+                                  batches, self.hidden_size,
                                   dtype=seq.dtype, device=seq.device)
-            c_zeros = torch.zeros(self.num_layers * num_directions,
-                                  1, self.hidden_size,
-                                  dtype=seq.dtype, device=seq.device)
-            hx = (h_zeros, c_zeros)
-            # if is_batched:
-            #     if (hx[0].dim() != 3 or hx[1].dim() != 3):
-            #         msg = ("For batched 3-D input, hx and cx should "
-            #                f"also be 3-D but got ({hx[0].dim()}-D, {hx[1].dim()}-D) tensors")
-            #         raise RuntimeError(msg)
-            # else:
-            #     if hx[0].dim() != 2 or hx[1].dim() != 2:
-            #         msg = ("For unbatched 2-D input, hx and cx should "
-            #                f"also be 2-D but got ({hx[0].dim()}-D, {hx[1].dim()}-D) tensors")
-            #         raise RuntimeError(msg)
-            #     hx = (hx[0].unsqueeze(1), hx[1].unsqueeze(1))
+            hx = h_zeros
 
-            # Each batch of the hidden state should match the input sequence that
-            # the user believes he/she is passing in.
-
-        samples = zip(*[w.sample_weights(torch.Size([batch_sz]), self.training) for w in self._weights])
-
-        outputs = []
-        hiddens = []
-        for x, weights in zip(seq, samples):
-            if x.is_cuda:
-                weights = torch._cudnn_rnn_flatten_weight(
-                    weights, 4,
-                    self.input_size, _cudnn.RNNMode.lstm,
-                    self.hidden_size, 0, self.num_layers,
-                    True, bool(self.bidirectional))
-            result = torch._VF.lstm(x.unsqueeze(dim=batch_dim), hx, weights, True, self.num_layers,
-                                    0, self.training, self.bidirectional, True)
-            outputs.append(result[0])
-            hiddens.append(result[1:])
-        output = torch.cat(outputs, dim=batch_dim)
-        hidden = (torch.stack([h[0].squeeze(1) for h in hiddens], dim=batch_dim),
-                  torch.stack([h[1].squeeze(1) for h in hiddens], dim=batch_dim))
-
-        # xxx: isinstance check needs to be in conditional for TorchScript to compile
+        if self.mode == "tanh":
+            if self.training:
+                outputs = []
+                hiddens = []
+                samples = zip(*[w.sample_weights(torch.Size([batch_sz]), self.training) for w in self._weights])
+                for x, weights in zip(seq, samples):
+                    if x.is_cuda and all([weight.is_cuda for weight in weights]):
+                        torch._cudnn_rnn_flatten_weight(
+                            weights, 4,
+                            self.input_size, int(_cudnn.RNNMode.lstm),
+                            self.hidden_size, 0, self.num_layers,
+                            True, bool(self.bidirectional))
+                    result = torch._VF.rnn_tanh(x.unsqueeze(dim=batch_dim), hx, weights, True, self.num_layers,
+                                                0, self.training, self.bidirectional, True)
+                    outputs.append(result[0])
+                    hiddens.append(result[1:])
+                output = torch.cat(outputs, dim=batch_dim)
+                hidden = (torch.stack([h[0].squeeze(1) for h in hiddens], dim=batch_dim),
+                          torch.stack([h[1].squeeze(1) for h in hiddens], dim=batch_dim))
+            else:
+                samples = (w.sample_weights(torch.Size([batch_sz]), self.training) for w in self._weights)
+                result = torch._VF.rnn_tanh(seq, hx, tuple(samples), True, self.num_layers,
+                                            0, self.training, self.bidirectional, True)
+                output = result[0]
+                hidden = result[1:]
+        elif self.mode == "relu":
+            if self.training:
+                outputs = []
+                hiddens = []
+                samples = zip(*[w.sample_weights(torch.Size([batch_sz]), self.training) for w in self._weights])
+                for x, weights in zip(seq, samples):
+                    if x.is_cuda and all([weight.is_cuda for weight in weights]):
+                        torch._cudnn_rnn_flatten_weight(
+                            weights, 4,
+                            self.input_size, int(_cudnn.RNNMode.lstm),
+                            self.hidden_size, 0, self.num_layers,
+                            True, bool(self.bidirectional))
+                    result = torch._VF.rnn_relu(x.unsqueeze(dim=batch_dim), hx, weights, True, self.num_layers,
+                                                0, self.training, self.bidirectional, True)
+                    outputs.append(result[0])
+                    hiddens.append(result[1:])
+                output = torch.cat(outputs, dim=batch_dim)
+                hidden = (torch.stack([h[0].squeeze(1) for h in hiddens], dim=batch_dim),
+                          torch.stack([h[1].squeeze(1) for h in hiddens], dim=batch_dim))
+            else:
+                samples = (w.sample_weights(torch.Size([batch_sz]), self.training) for w in self._weights)
+                result = torch._VF.rnn_relu(seq, hx, tuple(samples), True, self.num_layers,
+                                            0, self.training, self.bidirectional, True)
+                output = result[0]
+                hidden = result[1:]
+        else:
+            raise ValueError("Unrecognized RNN mode: " + self.mode)
         if not is_batched:
             output = output.squeeze(batch_dim)
             hidden = (hidden[0], hidden[1])
@@ -296,6 +436,9 @@ class RegEmbedding(torch.nn.Module):
     def reset_parameters(self):
         self.weight.reset_parameters()
 
+    def constrain_parameters(self):
+        self.weight.constrain_parameters()
+
     def set_lam(self, lam: float):
         self.lam = lam
         self.weight.set_lam(lam)
@@ -311,7 +454,7 @@ class RegEmbedding(torch.nn.Module):
         return self.weight.count_l0()
 
     def count_l2(self):
-        return self.weigt.count_l2()
+        return self.weight.count_l2()
 
     def forward(self, x: torch.LongTensor):
         '''
@@ -319,11 +462,11 @@ class RegEmbedding(torch.nn.Module):
         :return: Tensor of Size(x) x embedding_dimension
         '''
         if self.training:
-            embedding_samples = self.weight.sample_weights(x.size()[0], self.training)
+            embedding_samples = self.weight.sample_weights((x.size(0),), self.training)
             embeddings = [torch.nn.functional.embedding(x_i, emb_i) for x_i, emb_i in zip(x, embedding_samples)]
             return torch.stack(embeddings, dim=0)
         else:
-            embedding = self.weight.sample_weights(1, self.training).squeeze(dim=0)
+            embedding = self.weight.sample_weights((1,), self.training).squeeze(dim=0)
             return torch.nn.functional.embedding(x, embedding)
 
 
@@ -336,6 +479,10 @@ class RegLinear(torch.nn.Module):
     def reset_parameters(self):
         self.weight.reset_parameters()
         self.bias.reset_parameters()
+
+    def constrain_parameters(self):
+        self.weight.constrain_parameters()
+        self.bias.constrain_parameters()
 
     def set_lam(self, lam: float):
         self.lam = lam
@@ -354,7 +501,7 @@ class RegLinear(torch.nn.Module):
         return self.weight.count_l0() + self.bias.count_l0()
 
     def count_l2(self):
-        return self.weigt.count_l2() + self.bias.count_l2()
+        return self.weight.count_l2() + self.bias.count_l2()
 
     def forward(self, x: torch.LongTensor):
         if self.training:
@@ -369,3 +516,90 @@ class RegLinear(torch.nn.Module):
             weight = self.weight.sample_weights((1,), self.training).squeeze(dim=0)
             bias = self.bias.sample_weights((1,), self.training).squeeze(dim=0)
             return torch.nn.functional.linear(x, weight, bias)
+
+
+
+""" MODEL TESTS """
+
+
+# 1 - element test
+def test_1(n):
+
+    in_tensors = torch.randn(500, n)
+    target = torch.zeros_like(in_tensors)
+    target[:, 0] = in_tensors[:, 0]
+
+    model = RegLinear(n, n, .01)
+    opt = torch.optim.Adam(model.parameters())
+    model.train()
+
+    for i in range(10000):
+        opt.zero_grad()
+        loss = ((model(in_tensors) - target).pow(2)).mean()
+        re_loss = model.regularization()
+
+        total = loss + re_loss
+
+        total.backward()
+        opt.step()
+        model.constrain_parameters()
+        if i%10 == 0:
+            print(loss.item())
+            print(model.count_l0().item())
+
+    model.eval()
+    print((model(in_tensors) - target).pow(2).mean().item())
+
+
+# all-element test (n^2)
+def test_2(n):
+
+    in_tensors = torch.randn(500, n)
+    target = in_tensors.sum(dim=1).unsqueeze(dim=1).expand(-1, n)
+
+    model = RegLinear(n, n, .01)
+    model.train()
+    opt = torch.optim.Adam(model.parameters())
+
+    for i in range(10000):
+        opt.zero_grad()
+        loss = ((model(in_tensors) - target).pow(2)).mean()
+        re_loss = model.regularization()
+
+        total = loss + re_loss
+
+        total.backward()
+        opt.step()
+        if i%100 == 0:
+            print(loss.item())
+            print(model.count_l0().item())
+
+    model.eval()
+    print((model(in_tensors) - target).pow(2).mean().item())
+
+# identity test (n)
+def test_3(n):
+
+    in_tensors = torch.randn(500, n)
+    target = in_tensors
+
+    model = RegLinear(n, n, .01)
+    opt = torch.optim.Adam(model.parameters())
+
+    model.train()
+
+    for i in range(10000):
+        opt.zero_grad()
+        loss = ((model(in_tensors) - target).pow(2)).mean()
+        re_loss = model.regularization()
+
+        total = loss + re_loss
+
+        total.backward()
+        opt.step()
+        if i%100 == 0:
+            print(loss.item())
+            print(model.count_l0().item())
+
+    model.eval()
+    print((model(in_tensors) - target).pow(2).mean().item())
